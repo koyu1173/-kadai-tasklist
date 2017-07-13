9b6488614ec65ccea44d428e571decd0f04d20c0
koyufukunaga <koyu1173@yahoo.co.jp>
2017-07-13 11:03:41 +0000

    kadai-add-status

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,7 @@ class TasksController < ApplicationController
   end
   
   def show
-    @task = Task.find(params[:id])
+    set_task
   end
   
   def new
@@ -24,11 +24,11 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
+    set_task
   end
   
   def update
-    @task = Task.find(params[:id])
+    set_task
     if @task.update(task_params) 
       flash[:success] = 'タスクが正常に変更されました。'
       redirect_to @task
@@ -39,7 +39,7 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
+    set_task
     @task.destroy
 
     flash[:success] = 'タスク は正常に削除されました'
@@ -48,7 +48,11 @@ class TasksController < ApplicationController
   
   private
   
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end
